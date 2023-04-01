09dc00a02e9f0fdcbd37395f2f7333eca09a19ee
Mahmudov Mirabdulloh <mmirabdullo1999@gmail.com>
2023-04-01 06:38:37 +0500

    ci/cd automation is configured

--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -55,6 +55,7 @@ export class LessonService {
         include: { all: true },
       });
     } catch (error) {
+      console.log(error)
       if(!error.status){
         throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR)
       }
